7a8b033a0ec74e1d30f71b4d1c3d22f3b5086e1b
Matt Jones <mjon8807@yahoo.com>
2017-12-23 22:19:01 -0500

    simp

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+
+
+
+//function to load in JS and bring up the start button
+
+	
+var startQuiz = $("<button>");
+
+startQuiz.addClass('btn-large');
+startQuiz.addClass('btn');
+startQuiz.text('Start Quiz')
+
+$("body").append(startQuiz);
